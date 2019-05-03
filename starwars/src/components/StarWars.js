3e81dd52763e68a79ebd735ce734efa2f52c50ce
BrennanHolm <brennan.mholmich@gmail.com>
2019-05-03 11:38:20 -0500

    Expand component data
    Lists more data to be rendered

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -3,9 +3,18 @@ import './StarWars.css';
 
 const StarWars = (props) => {
   return (
-    <ul>
-      { props.starwarsChars.map(char => <li>{char.name}</li>) }
-    </ul>
+    <section className="section-list">
+      {props.starwarsChars.map(char =>
+      <ul className="sw-names">
+        <li className="char-names">{char.name}</li>
+        <ul>
+          <li className="char-att"><b>Birth Year: </b>{char.birth_year}</li>
+          <li className="char-att"><b>Gender: </b>{char.gender}</li>
+          <li className="char-att"><b>Height: </b>{char.height}</li>
+          <li className="char-att"><b>Mass: </b>{char.mass} kg</li>
+        </ul>
+      </ul>) }
+    </section>
   )
 }
 
